a99f29392098a0c1dc16c8119c05fc9abbafe761
Ipseeka Malla <ipseeka.malla1@gmail.com>
2023-08-21 10:11:42 +0545

    Signup auth from user side completed

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,57 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 const SignUp = () => {
+  const [credentials, setCredentials] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+    cpassword: "",
+    phoneNumber: "",
+    gender: "",
+  });
+  let navigate = useNavigate();
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitButton = async (e) => {
+    e.preventDefault();
+    const {
+      firstName,
+      lastName,
+      userName,
+      email,
+      password,
+      phoneNumber,
+      gender,
+    } = credentials;
+    const response = await fetch(`http://localhost:8848/api/auth/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        userName,
+        email,
+        password,
+        phoneNumber,
+        gender,
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      navigate("/login");
+    } else {
+      navigate("/");
+    }
+  };
+
   return (
     <div className="container h-100">
       <div className="row h-100 justify-content-center align-items-center">
@@ -8,7 +59,7 @@ const SignUp = () => {
           <div className="card shadow">
             <div className="card-body p-4">
               <h2 className="card-title text-center mb-4">Sign Up</h2>
-              <form>
+              <form onSubmit={handleSubmitButton}>
                 <div className="row mb-3">
                   <div className="col-md-6">
                     <label htmlFor="firstName" className="form-label">
@@ -18,26 +69,46 @@ const SignUp = () => {
                       type="text"
                       className="form-control"
                       id="firstName"
+                      onChange={onChange}
+                      name="firstName"
                     />
                   </div>
                   <div className="col-md-6">
                     <label htmlFor="lastName" className="form-label">
                       Last Name
                     </label>
-                    <input type="text" className="form-control" id="lastName" />
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="lastName"
+                      name="lastName"
+                      onChange={onChange}
+                    />
                   </div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="username" className="form-label">
                     Username
                   </label>
-                  <input type="text" className="form-control" id="username" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="userName"
+                    name="userName"
+                    onChange={onChange}
+                  />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="email" className="form-label">
                     Email
                   </label>
-                  <input type="email" className="form-control" id="email" />
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    onChange={onChange}
+                  />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="password" className="form-label">
@@ -47,6 +118,8 @@ const SignUp = () => {
                     type="password"
                     className="form-control"
                     id="password"
+                    name="password"
+                    onChange={onChange}
                   />
                 </div>
                 <div className="mb-3">
@@ -56,7 +129,9 @@ const SignUp = () => {
                   <input
                     type="password"
                     className="form-control"
-                    id="password"
+                    id="cpassword"
+                    name="cpassword"
+                    onChange={onChange}
                   />
                 </div>
 
@@ -65,30 +140,39 @@ const SignUp = () => {
                     <label htmlFor="phone" className="form-label">
                       Phone Number
                     </label>
-                    <input type="tel" className="form-control" id="phone" />
+                    <input
+                      type="tel"
+                      className="form-control"
+                      id="phoneNumber"
+                      name="phoneNumber"
+                      onChange={onChange}
+                    />
                   </div>
                   <div className="col-md-6">
                     <label htmlFor="gender" className="form-label">
                       Gender
                     </label>
-                    <select className="form-select" id="gender">
-                      <option value="male">Male</option>
-                      <option value="female">Female</option>
-                      <option value="other">Other</option>
+                    <select
+                      className="form-select"
+                      id="gender"
+                      name="gender"
+                      onChange={onChange}
+                    >
+                      <option value="Male">Male</option>
+                      <option value="Female">Female</option>
                     </select>
                   </div>
                 </div>
                 <button type="submit" className="btn btn-primary ms-auto">
                   Create Your Account
                 </button>
-                
               </form>
             </div>
             <div className="card-footer py-3 border-0">
               <div className="text-center">
-            Already have an account?{" "}
+                Already have an account?{" "}
                 <Link to="/login" className="text-dark">
-                 Login Here
+                  Login Here
                 </Link>
               </div>
             </div>
